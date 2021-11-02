22fcfb02b9cd8c1db823d826e548a6264a83305a
jhson <sjh1@cudo.co.kr>
2021-11-02 20:06:27 +0900

    환경설정 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1>Welcome to Home!!</h1>
+    <button type="button" @click="printenv">ENV LOG PRINT</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+
+  methods: {
+    printenv () {
+      console.log(process.env);
+    }
+  }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    devServer: {
+        port: process.env.VUE_APP_PORT || 8080 // SERVER PORT
+    },
+    publicPath: "/a/b" // BASE_URL
+}
