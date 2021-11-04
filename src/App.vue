277df51ab0ede4d0df9369c16227238191b90682
jhson <sjh1@cudo.co.kr>
2021-11-04 12:38:21 +0900

    App.vue sticky css 추가, Menu.vue component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <Header/>
+    <div class="sticky">
+      <Menu/>
+    </div>
     <div id="content" class="content">
       <router-view></router-view>
     </div>
@@ -8,21 +11,24 @@
   </div>
 </template>
 
+<style scoped>
+  .sticky {
+    position: sticky;
+    top: 0px;
+  }
+</style>
+
 <script>
 import Header from './components/layout/Header.vue'
+import Menu from './components/layout/Menu.vue'
 import Footer from './components/layout/Footer.vue'
 
 export default {
-  name: 'App',
-  components: {
+  name: 'App'
+  , components: {
       Header
+    , Menu
     , Footer
   }
 }
 </script>
-
-<style>
-#app {
-
-}
-</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <h1>Welcome to Main!!</h1>
-    <button type="button" @click="printenv">ENV LOG PRINT</button>
   </div>
 </template>
 
+<style scoped>
+div {
+  height: 2000px;
+}
+</style>
+
 <script>
 export default {
-  name: "Main",
-
-  methods: {
-    printenv () {
-      console.log(process.env);
-    }
-  }
+  name: "Main"
 }
 </script>
