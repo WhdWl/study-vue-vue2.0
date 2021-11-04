12123e1984ab1201c04e5579d7823036ca1c828c
jhson <sjh1@cudo.co.kr>
2021-11-04 17:07:20 +0900

    Box.vue state, props 추가

---
+++ src/components/content/Box.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-bind:class="['box', color]" v-bind:style="{width: width + 'px', height: height + 'px'}"></div>
+<!--  <div class="box" :style="{width: width + 'px', height: height + 'px'}"></div>-->
+</template>
+
+<style scoped>
+.box {
+  border: 1px solid #000;
+  margin: 20px;
+}
+.blue {
+  background: #009bff;
+}
+.purple {
+  background: #8f46ff;
+}
+.green {
+  background: #00bcac;
+}
+</style>
+
+<script>
+export default {
+  name: 'Box'
+  , props: {
+    color: {
+      type: String
+      , default: ""
+    }
+  }
+  , data() {
+    return {
+      width: 300
+      , height: 300
+    }
+  }
+}
+</script>
--- src/views/ContentTypeA.vue
+++ src/views/ContentTypeA.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
-    ContentTypeA
+    <Box color="blue"/>
+    <Box color="purple"/>
+    <Box color="green"/>
+    <Box/>
+    <Box color="blue"/>
+    <Box color="purple"/>
+    <Box color="green"/>
+    <Box/>
+    <Box color="blue"/>
+    <Box color="purple"/>
+    <Box color="green"/>
+    <Box/>
   </div>
 </template>
 
 <style scoped>
 div {
-  height: 500px;
-  text-align: center;
+  display: flex;
+  flex-direction: row; /* 왼쪽부터 가로로 정렬 */
+  /* flex-direction: row-reverse; 오른쪽부터 가로로 정렬 */
+  /* flex-direction: column; 위쪽부터 세로로 정렬 */
+  /* flex-direction: column-reverse; 아래쪽부터 세로로 정렬 */
+  justify-content: space-around; /* 자식요소들의 여백을 균등하게 정렬 */
+  /* justify-content: flex-start; 시작점에 맞춰 정렬 */
+  /* justify-content: flex-end; 끝점에 맞춰 정렬 */
+  /* justify-content: center; 가운데 정렬 */
+  /* justify-content: space-between; 여백을 균등하게 정렬 */
+  align-items: flex-start; /* 위에부터 수직방향으로 정렬 */
+  /* align-items: center; 가운데로부터 수직방향으로 정렬 */
+  /* align-items: flex-end; 아래부터 수직방향으로 정렬 */
+  flex-wrap: wrap;
 }
 </style>
 
 <script>
+import Box from '@/components/content/Box.vue'
+
 export default {
-  name: "ContentTypeA"
+  name: 'ContentTypeA'
+  , components: {
+    Box
+  }
 }
 </script>
