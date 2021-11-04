00825af7701a47ffbc83c27494dcc1041f51ad40
jhson <sjh1@cudo.co.kr>
2021-11-04 13:37:13 +0900

    Menu.vue css 추가

--- src/components/layout/Menu.vue
+++ src/components/layout/Menu.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    Menu
+  <div class="menu">
+    <div class="menu_left">
+      logo
+    </div>
+    <div class="menu_center">
+      menu
+    </div>
+    <div class="menu_right">
+      login
+    </div>
   </div>
 </template>
 
 <style scoped>
-div {
+.menu {
   height: 100px;
-  line-height: 100px;
   text-align: center;
   border-bottom: 1px solid #ebebeb;
+  display: flex;
+}
+.menu_left {
+  width: 100px;
+  line-height: 100px;
+  text-align: center;
+  border-right: 1px solid #ebebeb;
+}
+.menu_center {
+  width: 100%;
+  line-height: 100px;
+  text-align: center;
+  border-right: 1px solid #ebebeb;
+}
+.menu_right {
+  width: 300px;
+  line-height: 100px;
+  text-align: center;
 }
 </style>
 
