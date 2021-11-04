e3efebd805a68087e6d78b04050a5d2385415ad3
jhson <sjh1@cudo.co.kr>
2021-11-04 10:14:24 +0900

    publicPath 주석처리(process.env.BASE_URL)

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
     devServer: {
         port: process.env.VUE_APP_PORT || 8080 // SERVER PORT
     },
-    publicPath: "/a/b" // BASE_URL
+    // publicPath: "/a/b" // BASE_URL
 }
